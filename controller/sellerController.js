7a761cd82758947965f22735d3215ce7c3f3000b
KSudhaMadhuri <ksmdhuri736@gmail.com>
2024-09-29 15:21:54 +0530

    AddedSellerControllerandRoutes

--- controller/sellerController.js
+++ controller/sellerController.js
@@ -38,7 +38,7 @@ const loginSeller = async (req, res) => {
       res.status(404).json({ message: "User not found" });
     }
     const isMatch = await bcrypt.compare(password, exist.password);
-   // console.log(isMatch);
+    // console.log(isMatch);
     if (!isMatch) {
       res.status(400).json({ message: "Invalid password" });
     }
@@ -54,6 +54,8 @@ const loginSeller = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error", error });
   }
 };
+
+
 
 //getting sellerDetails
 const getSellerDetails = async (req, res) => {
@@ -71,4 +73,60 @@ const getSellerDetails = async (req, res) => {
   }
 };
 
-module.exports = { sellerSignUp, loginSeller, getSellerDetails };
+
+//get allsellers cotroller logic
+
+const getAllSellers = async(req,res)=>{
+  try {
+    const getSellers = await Seller.find();
+    if(!getSellers){
+      res.status(404).json({message:"No sellers found"})
+    }
+    res.status(200).json({message:"All sellers fetched successfully",getSellers})
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+}
+
+//updating sellerRole Controller logic
+const updateSellerRole = async (req, res) => {
+  try {
+    const { role } = req.body;
+    const updatedRole = await Seller.findByIdAndUpdate(
+      req.params.id,
+      { $set: { role: role } },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({ message: " Seller Role updated Successfully", updatedRole });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+//delete sellerController logic
+
+const deleteSeller = async (req, res) => {
+  try {
+    const deletedSeller = await Seller.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ message: "Deleted Seller Successfully", deletedSeller });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+module.exports = {
+  sellerSignUp,
+  loginSeller,
+  getSellerDetails,
+  updateSellerRole,
+  deleteSeller,
+  getAllSellers
+};
--- route/sellerRoute.js
+++ route/sellerRoute.js
@@ -1,9 +1,13 @@
-const express=require("express");
-const route= express.Router();
-const sellerController=require("../controller/sellerController");
-const verifyToken = require("../middleware")
+const express = require("express");
+const route = express.Router();
+const sellerController = require("../controller/sellerController");
+const verifyToken = require("../middleware");
 
-route.post("/signupseller",sellerController.sellerSignUp);
-route.post("/loginseller",sellerController.loginSeller);
-route.get("/getseller",verifyToken,sellerController.getSellerDetails);
-module.exports=route;
+route.post("/signupseller", sellerController.sellerSignUp);
+route.post("/loginseller", sellerController.loginSeller);
+route.get("/getallsellers",sellerController.getAllSellers);
+route.get("/getseller", verifyToken, sellerController.getSellerDetails);
+route.put("/updatesellerrole/:id", sellerController.updateSellerRole);
+route.delete("/deleteseller/:id", sellerController.deleteSeller);
+
+module.exports = route;
